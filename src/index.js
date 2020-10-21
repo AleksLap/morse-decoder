894bf40ece602544f2fa3cb36eb662a3cd7d2898
AleksLap <a.lapanovich@gmail.com>
2020-10-22 00:13:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,57 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprArr = expr.toString().split('');
+    let result = '';
+    let morseCode = '';
+    let encodedDotDas = '';
+    let step = 1;
+    let decimalCode = [];
+
+    function convertDecimalCode(arr) {
+        for (let i = 0; i < arr.length; i += step) {
+            if (arr[i] === '1') {
+                step = 2;
+            }
+            encodedDotDas = `${arr[i]}${arr[i + 1]}`
+            if(encodedDotDas === '10') {
+                morseCode += `.`;
+            } else if (encodedDotDas === '11') {
+                morseCode += `-`;
+            }
+        }
+    }
+
+    function getLetter() {
+        for (let code in MORSE_TABLE) {
+            if (code === morseCode) {
+                result += `${MORSE_TABLE[code]}`
+            }
+        }
+    }
+
+    for (let i = 0; i < exprArr.length; i++) {
+        decimalCode.push(exprArr[i]);
+
+        if (decimalCode.length === 10) {
+            step = 1;
+            if (decimalCode.indexOf('*') !== -1) {
+                result += ` `;
+                decimalCode = [];
+            } else {
+                convertDecimalCode(decimalCode);
+                getLetter();
+                decimalCode = [];
+                morseCode = '';
+            }
+        }
+    }
+    
+    return result;
 }
 
 module.exports = {
